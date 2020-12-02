74ad02741cf388a81100ba484f01fc56f75abe57
mahadyC <mahady.chowdhury@metropolia.fi>
2020-12-02 20:43:23 +0200

    Create HTML element and text node generating functions.

--- presenter.js
+++ presenter.js
@@ -4,9 +4,12 @@ import { view } from './view'
 let state = model;
 
 let render = () => {
-  console.log("hi");
-  view.paragraph.append("hello");
-  return document.querySelector("ul").appendChild(view.paragraph);
+  const list = document.querySelector("ul");
+  list.appendChild(view.listEl("itemName")).appendChild(view.divEl("itemName-div"));
+  const listItem = document.querySelector("li");
+  const itemDiv1 = document.getElementById("itemName-div");
+  itemDiv1.appendChild(view.checkboxEl(false));
+  itemDiv1.appendChild(view.textNode("item 1"));
 }
 
 export { render }
--- view.js
+++ view.js
@@ -1,6 +1,28 @@
-let paragraph = document.createElement("p");
-
+const listEl = (taskName) =>{
+  let el = document.createElement("li");
+  el.id = taskName;
+  return el;
+}
+const divEl = (id) =>{
+  let el = document.createElement("div");
+  el.id = id;
+  return el;
+}
+const checkboxEl = (complete) =>{
+  let el = document.createElement("input");
+  el.id = `checkbox-input-name`;
+  el.type = "checkbox";
+  el.checked = complete;
+  return el;
+}
+const textNode = (text) =>{
+  let textNd = document.createTextNode(text);
+  return textNd;
+}
 const view = {
-  paragraph: paragraph
+  listEl: listEl,
+  divEl: divEl,
+  checkboxEl: checkboxEl,
+  textNode: textNode
 }
 export { view }
