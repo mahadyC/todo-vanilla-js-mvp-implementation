88793147b277b9cfb7bd7f6193c859f9eaaa9462
mahadyC <mahady.chowdhury@metropolia.fi>
2020-12-04 12:49:47 +0200

    Detach UI from iterator to facilitate reusability of the UI and modularity of the code.

--- presenter.js
+++ presenter.js
@@ -52,55 +52,58 @@ activeTasksBtn.addEventListener("click", (event) => {
   const activeList = state.allTasks.filter(item => item.complete === false);
   console.log(activeList);
 });
+const allTasksUI = (item) => {
+  const list = document.querySelector("ul");
+  const listItem = view.listEl(item.name);
+  const itemDiv1 = view.divEl(`${item.name}Name-div`);
+  const checkBox = view.checkboxEl(item.complete, item.name);
+  const textNd = view.textNode(item.name);
+  const itemDiv2 = view.divEl(`${item.name}-rewrite-div`);
+  const editInp = view.editInputEl(item.name);
+  const saveBtn = view.buttonEl("Save", item.name);
+  const itemDiv3 = view.divEl(`${item.name}Edit-div`);
+  const editBtn = view.buttonEl("Edit", item.name);
+  const deleteBtn = view.buttonEl("Delete", item.name);
+  list.appendChild(listItem);
+  listItem.appendChild(itemDiv1);
+  itemDiv1.appendChild(checkBox);
+  itemDiv1.appendChild(textNd);
+  listItem.appendChild(itemDiv2);
+  itemDiv2.appendChild(editInp);
+  itemDiv2.appendChild(saveBtn);
+  listItem.appendChild(itemDiv3);
+  itemDiv3.appendChild(editBtn);
+  itemDiv3.appendChild(deleteBtn);
+  itemDiv2.hidden = true;
+  checkBox.addEventListener("change", (event) => {
+    const itemName = event.target.id.substring(9);
+    let checkedValue = event.target.checked;
+    updateComplete(itemName, checkedValue);
+  });
+  editBtn.addEventListener("click", (event) =>{
+    const item = document.getElementById(`${event.target.id.substring(12)}-rewrite-div`);
+    item.hidden ? item.hidden = false : item.hidden = true;
+  });
+  deleteBtn.addEventListener("click", (event) => {
+    const item = document.getElementById(`${event.target.id.substring(14)}`);
+    item.remove();
+    deleteItem(item.id);
+  });
+  saveBtn.addEventListener("click", (event) =>{
+    const item = document.getElementById(`${event.target.id.substring(12)}-rewrite-div`);
+    const previousName = document.getElementById(`${event.target.id.substring(12)}`).id;
+    const newName = item.firstChild.value;
+    const checkedValue = item.previousElementSibling.firstElementChild.checked ;
+    updateItemName(previousName, newName, checkedValue);
+    document.querySelector("ul").innerHTML = "";
+    render.allTasksState();
+  });
+  return listItem;
+}
 let render ={
   allTasksState: () => {
-   state.allTasks.map(item =>{
-      const list = document.querySelector("ul");
-      const listItem = view.listEl(item.name);
-      const itemDiv1 = view.divEl(`${item.name}Name-div`);
-      const checkBox = view.checkboxEl(item.complete, item.name);
-      const textNd = view.textNode(item.name);
-      const itemDiv2 = view.divEl(`${item.name}-rewrite-div`);
-      const editInp = view.editInputEl(item.name);
-      const saveBtn = view.buttonEl("Save", item.name);
-      const itemDiv3 = view.divEl(`${item.name}Edit-div`);
-      const editBtn = view.buttonEl("Edit", item.name);
-      const deleteBtn = view.buttonEl("Delete", item.name);
-      list.appendChild(listItem);
-      listItem.appendChild(itemDiv1);
-      itemDiv1.appendChild(checkBox);
-      itemDiv1.appendChild(textNd);
-      listItem.appendChild(itemDiv2);
-      itemDiv2.appendChild(editInp);
-      itemDiv2.appendChild(saveBtn);
-      listItem.appendChild(itemDiv3);
-      itemDiv3.appendChild(editBtn);
-      itemDiv3.appendChild(deleteBtn);
-      itemDiv2.hidden = true;
-      checkBox.addEventListener("change", (event) => {
-        const itemName = event.target.id.substring(9);
-        let checkedValue = event.target.checked;
-        updateComplete(itemName, checkedValue);
-      });
-      editBtn.addEventListener("click", (event) =>{
-        const item = document.getElementById(`${event.target.id.substring(12)}-rewrite-div`);
-        item.hidden ? item.hidden = false : item.hidden = true;
-      });
-      deleteBtn.addEventListener("click", (event) => {
-        const item = document.getElementById(`${event.target.id.substring(14)}`);
-        item.remove();
-        deleteItem(item.id);
-      });
-      saveBtn.addEventListener("click", (event) =>{
-        const item = document.getElementById(`${event.target.id.substring(12)}-rewrite-div`);
-        const previousName = document.getElementById(`${event.target.id.substring(12)}`).id;
-        const newName = item.firstChild.value;
-        const checkedValue = item.previousElementSibling.firstElementChild.checked ;
-        updateItemName(previousName, newName, checkedValue);
-        document.querySelector("ul").innerHTML = "";
-        render.allTasksState();
-      });
-      return listItem;
+   state.allTasks.forEach(element =>{
+      allTasksUI(element);
     }); 
   }
 }
